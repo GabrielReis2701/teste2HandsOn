75395e6b9df7b82dba3e9302e26d5a4ee6cd35a4
Silas P Ladislau <silas.ladislau@ale.ro.gov.br>
2022-04-13 11:51:37 -0400

    remove deprecated methods

--- app/src/main/java/com/example/teste2handson/MainActivity.java
+++ app/src/main/java/com/example/teste2handson/MainActivity.java
@@ -1,70 +1,71 @@
 package com.example.teste2handson;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.hardware.SensorListener;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements SensorListener {
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.teste2handson.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     final String tag = "MainActivity";
-    SensorManager sm = null;
-    TextView xViewA = null;
-    TextView yViewA = null;
-    TextView zViewA = null;
-    TextView xViewO = null;
-    TextView yViewO = null;
-    TextView zViewO = null;
-    /** Called when the activity is first created. */
+    private ActivityMainBinding binding;
+    SensorManager mSensorManager = null;
+    Sensor accelerometerSensor;
+    Sensor rotationSensor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         // get reference to SensorManager
-        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
-        xViewA = (TextView) findViewById(R.id.xbox);
-        yViewA = (TextView) findViewById(R.id.ybox);
-        zViewA = (TextView) findViewById(R.id.zbox);
-        xViewO = (TextView) findViewById(R.id.xboxo);
-        yViewO = (TextView) findViewById(R.id.yboxo);
-        zViewO = (TextView) findViewById(R.id.zboxo);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        rotationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
     }
-    public void onSensorChanged(int sensor, float[] values) {
-        synchronized (this) {
-            Log.d(tag, "onSensorChanged: " + sensor + ", x: " +
-                    values[0] + ", y: " + values[1] + ", z: " + values[2]);
-            if (sensor == SensorManager.SENSOR_ORIENTATION) {
-                xViewO.setText("Orientation X: " + values[0]);
-                yViewO.setText("Orientation Y: " + values[1]);
-                zViewO.setText("Orientation Z: " + values[2]);
-            }
-            if (sensor == SensorManager.SENSOR_ACCELEROMETER) {
-                xViewA.setText("Accel X: " + values[0]);
-                yViewA.setText("Accel Y: " + values[1]);
-                zViewA.setText("Accel Z: " + values[2]);
-            }
-        }
-    }
-    public void onAccuracyChanged(int sensor, int accuracy) {
-        Log.d(tag,"onAccuracyChanged: " + sensor + ", accuracy: " + accuracy);
-    }
+
     @Override
     protected void onResume() {
         super.onResume();
-        // register this class as a listener for the orientation and accelerometer sensors
-        sm.registerListener(this,
-                SensorManager.SENSOR_ORIENTATION |SensorManager.SENSOR_ACCELEROMETER,
+        mSensorManager.registerListener(this, accelerometerSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+        mSensorManager.registerListener(this, rotationSensor,
                 SensorManager.SENSOR_DELAY_NORMAL);
     }
+
     @Override
-    protected void onStop() {
-        // unregister listener
-        sm.unregisterListener(this);
-        super.onStop();
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this, accelerometerSensor);
+        mSensorManager.unregisterListener(this, rotationSensor);
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Log.d(tag, "onSensorChanged: " + event.sensor.getStringType() + ", x: " +
+                event.values[0] + ", y: " + event.values[1] + ", z: " + event.values[2]);
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            binding.rotationX.setText("Rotation X: " + event.values[0]);
+            binding.rotationY.setText("Rotation Y: " + event.values[1]);
+            binding.rotationZ.setText("Rotation Z: " + event.values[2]);
+        }
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            binding.accelerometerX.setText("Accel X: " + event.values[0]);
+            binding.accelerometerY.setText("Accel Y: " + event.values[1]);
+            binding.accelerometerZ.setText("Accel Z: " + event.values[2]);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.d(tag,"onAccuracyChanged: " + sensor + ", accuracy: " + accuracy);
+    }
 }
